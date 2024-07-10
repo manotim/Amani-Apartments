7ad3516f8d34f13e12929c68373b92262d33d077
manoti <52492406+manotim@users.noreply.github.com>
2024-07-10 05:20:07 +0300

    add site logo in header

--- gaproject/gaproject/settings.py
+++ gaproject/gaproject/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
